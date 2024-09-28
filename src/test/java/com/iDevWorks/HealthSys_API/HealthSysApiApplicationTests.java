70fb47149c041ad1803268340caf0144584ec0b8
Daniel Maldonado <daniel.maldonado95@outlook.com>
2024-09-28 17:30:11 -0600

    Feature: Project architecture.

---
+++ src/test/java/com/iDevWorks/HealthSys_API/HealthSysApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iDevWorks.HealthSys_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthSysApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
